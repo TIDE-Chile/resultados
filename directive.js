d8b327b24d462fa0993365d74133553bc665ba44
ismaeluct2013 <isoto2010@alu.uct.cl>
2015-02-10 17:15:53 -0300

    linea de tiempo + bug de cuadros de color no horizontal
    se agrega la linea de tiempo grafica y tbn se presenta una solucion a la
    parte de preseleccion donde tiene pocas becas quedaban los colores
    informativos de gorma horizontal

--- directive.js
+++ directive.js
@@ -50,15 +50,15 @@ angular.module('tideApp')
         var popover = element.data('bs.popover');
 
 
-        if (typeof popover !== "undefined") {
+    if (typeof popover !== "undefined") {
             var $tip = popover.tip();
             var zindex = $tip.css('z-index');
             
             $tip.find('.close').bind('click', function () {
                 popover.hide();
             });
-        /*en prueba aun falta afinar mas esto */
-         $("body").on('click',function(){
+            /*en prueba aun falta afinar mas esto */
+            $("body").on('click',function(){
                 popover.hide();
              });
 
@@ -78,6 +78,62 @@ angular.module('tideApp')
 
             
     });
+    }
+  };
+}]);
+
+/*
+* tdTimeline
+*/
+angular.module('tideApp')
+.directive('tdTimeline', [ '$templateCache', '$compile', '$http', function ( $templateCache, $compile, $http ) {
+  return {
+    restrict: 'A',
+    scope: {
+        etapa: "=?ngModel", // etapa seleccionada
+        infoEtapas: "=?tdInfoEtapas",  // objetos de datos para usarlo en scope del compile
+        templateUrl: "=?tdTemplateUrl"
+    },
+    link: function(scope, element) {
+        /*
+        * selectEtapa
+        * Función que se llama al hacer click sobre alguna de las etapas
+        * Cambia la clase de la etapa a "active" y modifica el ng-model con el id de la etapa
+        */
+        var selectEtapa = function(e) {
+            var $etapaSeleccionada = angular.element(this);
+            var idEtapa = $etapaSeleccionada.attr("id");
+
+            // Elimina clase "active" de todas las etapas
+            var $etapas = angular.element(element[0].querySelectorAll(".etapa"));
+            $etapas.attr("class","etapa");
+
+            // Agrega clase "active" a etapa seleciconada
+            $etapaSeleccionada.attr("class", "etapa active");
+
+            scope.$apply(function() {
+                scope.etapa = idEtapa;
+            })
+
+        }
+
+        // Obtener template desde archivo externo si se define atributo td-template-url
+        if (scope.templateUrl) {
+            $http.get(scope.templateUrl)
+            .then(function(response) {
+              var template = $templateCache.put( 'templateBeneficio.html', response.data );
+              var content= $compile($(template))(scope);
+              element.html(content);
+
+              var $etapas = angular.element(element[0].querySelectorAll(".etapa"));
+              // Asocia evento "on click" a cada elemento de clase "etapa"
+              $etapas.on('click', selectEtapa);
+              $etapas.tooltip({container: 'body',placement: 'auto'});
+
+              var $etapaSeleccionada = angular.element(element[0].querySelectorAll("#"+scope.etapa+".etapa"));
+              $etapaSeleccionada.attr("class", "etapa active");
+            }) 
+        } 
     }
   };
 }]);
