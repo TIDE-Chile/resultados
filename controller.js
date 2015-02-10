036f5520842e950553f9324e35c49ab949fca45b
Ernesto Laval <ernesto.laval@gmail.com>
2015-02-10 23:36:42 +0000

    RUT configurable en url (#31)
    Se puede especificar RUT a consultar en el URL

--- controller.js
+++ controller.js
@@ -14,19 +14,51 @@
  * @property {object} data Objeto con datos de resultados para un postulante
  * @description
  *
- * Controller for Carreras explorer
+ * Controller for App Resultados
  *
  */
 angular.module('tideApp')
-.controller('AppController', ['$scope','$modal','DataService',function ($scope, $modal, dataService) {
+.controller('AppController', ['$scope','DataService',function ($scope, dataService) {
   var myself = this;
 
-  this.rut = "12345678";
+  // 
   this.estadoSeleccionado = {}
 
  /*
+  * QueryString
+  * Función auxiliar que permite obtener parametros del url
+  */
+  // Aux function to get url GET params
+  var QueryString = function () {
+    // This function is anonymous, is executed immediately and 
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+        // If first entry with this name
+      if (typeof query_string[pair[0]] === "undefined") {
+        query_string[pair[0]] = pair[1];
+        // If second entry with this name
+      } else if (typeof query_string[pair[0]] === "string") {
+        var arr = [ query_string[pair[0]], pair[1] ];
+        query_string[pair[0]] = arr;
+        // If third or later entry with this name
+      } else {
+        query_string[pair[0]].push(pair[1]);
+      }
+    } 
+      return query_string;
+  } ();
+
+  // Parámetro (RUT) siendo buscado
+  this.rut = QueryString.rut ? QueryString.rut : "12345678";
+
+
+ /*
   * iniciaEpataSeleccionada
-  * Asigna valores iniciales para 
+  * Asigna valores iniciales para la etapa seleccionada de cada proceso
   */
   var iniciaEtapaSeleccionada = function(data) {
     if (data && data.misBeneficios && data.misBeneficios.categorias) {
@@ -39,7 +71,8 @@ angular.module('tideApp')
     }
   }
 
- this.isOpenAccordion = {
+  // Mantiene el estado abierto/cerrado de cada bloque del acordeón
+  this.isOpenAccordion = {
     mensajesDestacados: true,
     datosPostulante: false,
     misBeneficios : false
